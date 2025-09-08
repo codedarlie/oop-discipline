0aa8048ce0f8d9c6da24eb98ec0ce32fe029708d
codedarlie <helloxking@gmail.com>
2025-09-08 19:17:16 +0500

    Realize composition,new methods of based and derived classes, add examples and other

--- university/semester6/oop/laboratoryWork2/main.cpp
+++ university/semester6/oop/laboratoryWork2/main.cpp
@@ -3,116 +3,98 @@
 
 class Matrix {
 protected:
-    unsigned int rows;
-    unsigned int columns;
+    size_t rows;
+    size_t columns;
     std::vector<std::vector<int>> data;
 
 public:
-    Matrix(const unsigned int r, const unsigned int c, const int value = 0): rows{r}, columns{c} {
-        std::cout << this << ": Matrix(r, c, value)\n";
+    Matrix(): rows{0}, columns{0} {
+        std::cout << "Matrix::Constructor()\n";
+    }
+    Matrix(size_t r, size_t c, const int v = 0): rows{r}, columns{c} {
+        std::cout << "Matrix::Constructor(rows, columns, value)\n";
         data = std::vector<std::vector<int>> (r);
         for (auto& i: data) {
-            i = std::vector<int> (c, value);
+            i = std::vector<int> (c, v);
         }
     }
-    Matrix(const int value = 0): Matrix(1, 1, value) {};
+    Matrix(const int value): Matrix(1, 1, value) {
+        std::cout << "Matrix::Constructor(value)\n";
+    };
     Matrix(const Matrix& m): rows{m.rows}, columns{m.columns}, data{m.data} {
-        std::cout << this << ": Matrix(const Matrix& m)\n";
+        std::cout << "Matrix::Copy Constructor\n";
     }
-    Matrix(Matrix&& m): rows{std::move(m.rows)}, columns{std::move(m.columns)}, data{std::move(m.data)} {
-        std::cout << this << ": Matrix(Matrix&& m)\n";
+    Matrix(Matrix&& m): rows{m.rows}, columns{m.columns}, data{std::move(m.data)} {
+        std::cout << "Matrix::Move Constructor\n";
         m.rows = 0;
         m.columns = 0;
     } 
     
-    void setValue(const unsigned int i, const unsigned int j, const int value) {
-        std::cout << this << ": setValue(i, j, value)\n";
-        if (i < rows && j < columns) {
-            data[i][j] = value;
-            return;
-        }
-        throw;
-    }
-    void setRangeValue(const unsigned int i1, const unsigned int i2, const unsigned int j1, const unsigned int j2, const int value) {
-        if ((i1 >= rows) || (i2 > rows) || (j1 >= columns) || (j2 > columns)) throw;
+    void setValue(size_t i, size_t j, const int value) {
+        std::cout << "Matrix::setValue()\n";
+        if (i >= rows || j >= columns) throw std::out_of_range("Ошибка: Вышли за границы.\n");
+        data[i][j] = value;
+        return;
+    }   
+    void setRangeValue(size_t i1, size_t i2, size_t j1, size_t j2, const int value) {
+        std::cout << "Matrix::setRangeValue()\n";
+        if ((i1 >= rows) || (i2 > rows) || (j1 >= columns) || (j2 > columns)) throw std::out_of_range("Ошибка: Вышли за границы.\n");
         
-        for (int i = i1; i < i2; ++i) {
-            for (int j = j1; j < j2; ++j) {
+        for (size_t i = i1; i < i2; ++i) {
+            for (size_t j = j1; j < j2; ++j) {
                 data[i][j] = value;
             }
         }
     }
-    const int getValue(const unsigned int i, const unsigned int j) const {
-        std::cout << this << ": getValue(i, j)\n";
-        if (!(i < rows && j < columns)) {
-            throw;
-        }
+    const int getValue(size_t i, size_t j) const {
+        std::cout << "Matrix::getValue() const\n";
+        if (i >= rows || j >= columns) throw std::out_of_range("Ошибка: Вышли за границы.\n");
+        return data[i][j];
+    }
+    int& getValue(size_t  i, size_t j) {
+        std::cout << "Matrix::getValue()\n";
+        if (i >= rows || j >= columns) throw std::out_of_range("Ошибка: Вышли за границы.\n");
         return data[i][j];
     }
     size_t getRowsSize() const {
+        std::cout << "Matrix::getRowsSize()\n";
         return rows;
     }
     size_t getColumnsSize() const {
+        std::cout << "Matrix::getColumnsSize()\n";
         return columns;
     }
+    const std::vector<std::vector<int>>& getData() const {
+        std::cout << "Matrix::getData()\n";
+        return data;
+    }
+    
     bool isEmpty() const;
-
     virtual void transpose() {
-        std::cout << this << ": transpose()\n";
+        std::cout << "Matrix::transpose()\n";
+
         std::vector<std::vector<int>> data2(columns);
-        for (int i = 0; i < data2.size(); ++i) {
+        for (size_t i = 0; i < data2.size(); ++i) {
             data2[i] = std::vector<int> (rows);
-            for (int k = 0; k < data2[i].size(); ++k) {
+            for (size_t k = 0; k < data2[i].size(); ++k) {
                 data2[i][k] = data[k][i];
             }
         }
         data = std::move(data2);
+
         std::swap(columns, rows);
     }
-    void fill(const int value) {
-        for (auto& i: data) {
-            for (auto& k: i) k = value;
-        }
-        
-    }
-    virtual void resize(const unsigned r, const unsigned c, const int value = 0) {
-        if (!r && c || r && !c) throw;
-        data.resize(r);
-        for (int i = rows; i < r; ++i) {
-            data[i] = std::vector<int> (columns, value);
-        }
-        for (auto& i: data) {
-            i.resize(c);
-            for (int k = columns; k < c; ++k) {
-                i[k] = value;
-            }
-        }
-        columns = c;
-        rows = r;
-    }
-    
-    void swap(const unsigned i1, const unsigned j1, const unsigned i2, const unsigned j2) {
-        if (!(i1 < rows && i1 < rows && j1 < columns && j2 < columns)) {
-            throw;
-        }
-        std::swap(data[i1][j1], data[i2][j2]);
-    }
-    void clear() {
-        data.clear();
-        rows = 0;
-        columns = 0;
-    }
-
-    Matrix minor(const unsigned int i, const unsigned int j) {
-        if (i >= rows || j >= columns) throw;
+    Matrix minor(size_t i, size_t j) {
+        std::cout << "Matrix::minor()\n";
+        if (i >= rows || j >= columns) throw std::out_of_range("Ошибка: Вышли за границы.\n");
 
         Matrix m(rows - 1, columns - 1);
 
         int m_i {0};
-        for (int k = 0; k < rows; ++k) {
+        for (size_t k = 0; k < rows; ++k) {
             if (k == j) continue;
             int m_j {0};
-            for (int q = 0; q < columns; ++q) {
+            for (size_t q = 0; q < columns; ++q) {
                 if (q == i) continue;
                 m.data[m_i][m_j] = data[k][q];
                 ++m_j;
@@ -121,11 +103,49 @@ public:
         }
         return m;
     }
+    void fill(const int value) {
+        std::cout << "Matrix::fill()\n";
+        for (auto& i: data) {
+            for (auto& k: i) k = value;
+        }
+        
+    }
+    virtual void resize(size_t r, size_t c, const int value = 0) {
+        if (!r && c || r && !c) throw std::out_of_range("Ошибка: Вышли за границы.\n");
+
+        std::cout << "Matrix::resize()\n";
+
+        data.resize(r);
+        for (size_t i = rows; i < r; ++i) {
+            data[i] = std::vector<int> (columns, value);
+        }
+        for (auto& i: data) {
+            i.resize(c);
+            for (size_t k = columns; k < c; ++k) {
+                i[k] = value;
+            }
+        }
+        columns = c;
+        rows = r;
+    }
+    void clear() {
+        std::cout << "Matrix::clear()\n";
+        data.clear();
+        rows = 0;
+        columns = 0;
+    }
+    void swap(size_t i1, size_t j1, size_t i2, size_t j2) {
+        std::cout << "Matrix::swap()\n";
+        
+        if (!(i1 < rows && i1 < rows && j1 < columns && j2 < columns)) throw std::out_of_range("Ошибка: Вышли за границы.\n");
+        
+        std::swap(data[i1][j1], data[i2][j2]);
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
 
     virtual ~Matrix() {
-        std::cout << this << ": ~Matrix()\n";
+        std::cout << "~Matrix::Destructor\n";
         rows = 0;
         columns = 0;
         data.clear();
@@ -133,8 +153,10 @@ public:
 };
 
 bool Matrix::isEmpty() const {
+    std::cout << "Matrix::isEmpty()\n";
     return data.empty();
 }
+
 std::ostream& operator<<(std::ostream& os, const Matrix& m) {
     os << "Matrix (" << m.rows << ", " << m.columns << "):\n";
     for (auto& i: m.data) {
@@ -148,76 +170,64 @@ std::ostream& operator<<(std::ostream& os, const Matrix& m) {
 }
 
 class SquareMatrix: public Matrix {
-private:
-    using Matrix::resize;
-
 public:
     SquareMatrix(const unsigned int i = 1, const int value = 0): Matrix(i, i, value) {
-        std::cout << this << ": SquareMatrix(i, value)\n";
+        std::cout << "SquareMatrix(i, value)\n";
     }
     SquareMatrix(const SquareMatrix& m): Matrix(m) {
-        std::cout << this << ": SquareMatrix(const SquareMatrix&)\n";
+        std::cout << "SquareMatrix(const SquareMatrix&)\n";
     }
     SquareMatrix(SquareMatrix&& m): Matrix(std::move(m)) {
-        std::cout << this << ": SquareMatrix(SquareMatrix&&)\n";
+        std::cout << "SquareMatrix(SquareMatrix&&)\n";
     }
 
     size_t getSize() const {
-        std::cout << this << ": getSize()\n";
+        std::cout << "getSize()\n";
         return rows;
     }
     
     void transpose() {
-        for (int i = 0; i < rows; ++i) {
-            for (int k = 1 + i; k < rows; ++k) {
+        std::cout << "SquareMatrix::transpose()\n";
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t k = 1 + i; k < rows; ++k) {
                 swap(i, k, k, i);
             }
         }
-        
-        /*  2 3 4 5 6
-        5 3 1 3 2
-        1 2 3 4 5
-        2 3 4 5 9
-        1 3 2 1 3  */
     }
     const int trace() const {
-        if (!rows) throw;
+        if (!rows) throw std::out_of_range("Ошибка: Вышли за границы.\n");
 
         int num {0};
-        for (int i = 0; i < rows; ++i) {
+        for (size_t i = 0; i < rows; ++i) {
             num += data[i][i];
         }
         return num;
     }
     
-    void resize(const unsigned r, const int value = 0) {
+    void resize(const unsigned r, const unsigned c, const int value = 0) {
+        std::cout << "SquareMatrix::resize()\n";
+        if (r != c) throw std::invalid_argument("Ошибка: rows должен быть равен columns.\n");
+
         Matrix::resize(r, r, value);
     }
     void makeIdentity() {
-        std::cout << this << ": makeIdentity()\n";
+        std::cout << "SquareMatrix::makeIdentity()\n";
         
-        for (int i = 0; i < data.size(); ++i) {
-            for (int k = 0; k < data[i].size(); ++k) {
+        for (size_t i = 0; i < data.size(); ++i) {
+            for (size_t k = 0; k < data[i].size(); ++k) {
                 if (i == k) data[i][k] = 1;
                 else data[i][k] = 0;
             }
         }
-        std::cout << *this;
     }
     
-    
-    // Переделать.
-    // size_t getRowsSize() const = delete;
-    // size_t getColumnsSize() const = delete;
-    
-    // const int determinant() const;   // Нахождение детерминанты
-    // SquareMatrix inverse() const;   // Нахождение обратной матрицы
-    
+    size_t getRowsSize() const = delete;
+    size_t getColumnsSize() const = delete;
     
     friend std::ostream& operator<<(std::ostream& os, const SquareMatrix& m);
     
     ~SquareMatrix() {
-        std::cout << this << ": ~SquareMatrix()\n";
+        std::cout << "~SquareMatrix()\n";
     }
 };
 
@@ -229,6 +239,129 @@ std::ostream& operator<<(std::ostream& os, const SquareMatrix& m) {
         }
         os << '\n';
     }
+    return os;
+}
+
+class MatrixCalculator {
+private:
+    const Matrix* m1;
+    const Matrix* m2;
+
+    enum class Operations {addition, substraction, multiplication, divide};
+    
+    void operate(Operations op, Matrix& m) const {
+        int m1c = m1->getColumnsSize();
+        int m1r = m1->getRowsSize();
+        int m2c = m2->getColumnsSize();
+        int m2r = m2->getRowsSize();
+        if ((m1c != m2c) || (m1r != m2r) || !m1c || !m1r) throw std::runtime_error("Ошибка: Матрицы разного размера.\n");
+
+        m.resize(m1c, m1r);
+        for (size_t i = 0; i < m1c; ++i) {
+            for (size_t k = 0; k < m1r; ++k) {
+                int value {0};
+
+                switch (op)
+                {
+                case Operations::addition:
+                    value = m1->getValue(i, k) + m2->getValue(i, k);
+                    break;
+                case Operations::substraction:
+                    value = m1->getValue(i, k) - m2->getValue(i, k);
+                    break;
+                case Operations::multiplication:
+                    value = m1->getValue(i, k) * m2->getValue(i, k);
+                    break;
+                case Operations::divide:
+                    if (!m2->getValue(i, k)) throw std::runtime_error("Ошибка: деление на ноль.\n");
+                    value = m1->getValue(i, k) / m2->getValue(i, k);
+                    break;
+
+                default:
+                    throw std::runtime_error("Ошибка: Не выбрана операция");
+                    break;
+                }
+
+                m.setValue(i, k, value);
+            }
+        }
+    }
+
+public:
+    MatrixCalculator(): m1{new Matrix()}, m2{new Matrix()} {
+        std::cout << "MatrixCalculator()\n";
+    } 
+    MatrixCalculator(const Matrix* pm1, const Matrix* pm2): m1{new Matrix(*pm1)}, m2{new Matrix(*pm2)} {
+        std::cout << "MatrixCalculator(Matrix*, Matrix*)\n";
+    }
+    MatrixCalculator(const unsigned int rows, const unsigned int columns, const int value = 0): m1{new Matrix(rows, columns, value)}, m2{new Matrix(rows, columns, value)} {
+        std::cout << "MatrixCalculator(rows, columns, value)\n";
+    }
+
+    void addition(Matrix& m) {
+        operate(Operations::addition, m);
+    }
+    void substraction(Matrix& m) {
+        operate(Operations::substraction, m);
+    }
+    void multiplication(Matrix& m) {
+        operate(Operations::multiplication, m);
+    }
+    void divide(Matrix& m) {
+        operate(Operations::divide, m);
+    }
+
+    ~MatrixCalculator() {
+        std::cout << "~MatrixCalculator()\n";
+        delete m1;
+        delete m2;
+    }
+};
+
+class MatrixWrapper {
+private:
+    std::string name;
+    std::string description;
+    Matrix m;
+
+public:
+    MatrixWrapper(const MatrixWrapper& mw): m{mw.m}, name{mw.name}, description{mw.description} {
+        std::cout << "MatrixWrapper constructor: " << name << "(mw)\n";
+    }
+    MatrixWrapper(const Matrix& matrix, const std::string& n = "Unnamed", const std::string& d = "No Description"): m{matrix}, name{n}, description{d} {
+        std::cout << "MatrixWrapper constructor: " << name << "(matrix)\n";
+    }
+
+    const Matrix& getMatrix() const {
+        return m;
+    }
+    std::string getName() const {
+        return name;
+    }
+    void setDescription(const std::string& d) {
+        description = d; 
+    }
+    void setName(const std::string& n) {
+        name = n; 
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const MatrixWrapper& m);
+
+    ~MatrixWrapper() {
+        std::cout << "~MatrixWrapper: " << name << '\n';
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const MatrixWrapper& m) {
+    os << "\n——————————————————————\n| MatrixWrapper:\n| Name: " << m.name << "\n| Description: " << m.description << "\n| Size: " << m.m.getRowsSize() << ", " << m.m.getColumnsSize() << "\n| Inside:\n";
+    for (auto& i: m.m.getData()) {
+        os << "| ";
+        for (auto& k: i) {
+            os << k << ' ';
+        }
+        os << '\n';
+    }
+    os << "——————————————————————\n\n";
     return os;
 }
 
@@ -244,10 +377,12 @@ int main() {
     c << m3;
     Matrix* m4 = new Matrix(4, 3, 10);
     c << *m4;
+    delete m4;
     Matrix m5 = m3;
     c << m5;
     Matrix m6 = std::move(m5);
     c << m6;
+
 
     // PART 2: Methods
     m6.setValue(1, 2, 341);
@@ -281,6 +416,7 @@ int main() {
     Matrix m7 = m6.minor(4, 6);
     c << m7;
 
+
     // PART 3: Derived Class Constructors
     SquareMatrix s1;
     c << s1;
@@ -288,6 +424,7 @@ int main() {
     c << s2;
     SquareMatrix s3 {std::move(s2)};
     SquareMatrix s4 {s3};
+
 
     // PART 4: Derived Class Methods
     s4.fill(6);
@@ -297,8 +434,40 @@ int main() {
     c << s4;
     s4.getSize();
     s4.clear();
-    s4.resize(7, 3);
+    s4.resize(7, 7, 3);
     c << s4;
+
+    // PART 5: Polymophism
+    Matrix* p = new SquareMatrix();
+
+    p->resize(7, 7, 6);
+    p->setRangeValue(0, 4, 0, 1, 5);
+    p->setRangeValue(0, 4, 1, 3, 3);
+    p->setRangeValue(0, 3, 3, 6, 8);
+    p->setRangeValue(3, 4, 3, 6, 9);
+    c << *p;
+    p->transpose();
+    c << *p;
+    p->transpose();
+
+
+    // PART 6: Compositions (Matrix* and Matrix)
+    Matrix* p2 = new SquareMatrix(s4);
+    c << *p2;
+    MatrixCalculator mc (p, p2);
+    Matrix* pr = new SquareMatrix();
+    mc.substraction(*pr);
+    c << *pr;
+    delete p2;
+    delete p;
+
+    MatrixWrapper* mw2 = new MatrixWrapper(*pr, "SquareMatrix", "Substraction Result");
+    c << *mw2;
+    Matrix m8 = mw2->getMatrix();
+    c << m8;
+
+    delete pr;
+    delete mw2;
 
     return 0;
 }
